b1dad68260be74c3db9d75d308efe0300cd7ec9c
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-01-04 21:02:45 -0500

    commit tree 196154f2e29137eea9a0cead592b2ad42f9689f9

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def list_roles
+        self.characters.map do |character|
+            "#{character.name} - #{character.show.name}"
+        end
+    end
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
 end
